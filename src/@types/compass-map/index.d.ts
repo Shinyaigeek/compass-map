b35a9bb8132e4e7675d58e7300b726f8d57e8328
Shinobu Hayashi <42742053+PhilisPaxil@users.noreply.github.com>
2019-08-11 17:04:43 +0900

    [add]type of SearchQuery

--- src/@types/compass-map/index.d.ts
+++ src/@types/compass-map/index.d.ts
@@ -4,4 +4,19 @@ interface StoreState {
 	prefecture: string;
 }
 
+interface SearchQuery {
+    event_id:number[],
+    keyword:string[],
+    keyword_or:string[],
+    ym:number[],
+    ymd:number[],
+    nickname:string[],
+    owner_nickname:string[],
+    series_id:number[],
+    start:number;
+    order:number,
+    cound:number,
+    format:"json"
+}
+
 export { StoreState };
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,32 +1,33 @@
-
 interface PostData {
-    "model":string;
-    "article": string;
-  }
-  
-  export function postContents(content:string,model:string){
-    const postModel = model;
-      const data:PostData = {
-          "model" : postModel,
-          "article": content
-      }
-      return postData(`http://18.182.126.246/api/demo`, data)
-      // return postData(`https://13.230.60.72/api01`,data)
-  }
-  
-  // POSTメソッドの実装
-  
-  function postData(url = ``, data = {}) {
-      return fetch(url,{
-          method: "POST", 
-          mode:"cors",
-          credentials: "same-origin",
-          headers: {
-              "Content-Type": "text/plain",
-          },
-          redirect: "follow",
-          referrer: "no-referrer",
-          body: JSON.stringify(data),
-      })
-      .then(response => response.text());
-  }
+	model: string;
+	article: string;
+}
+
+export function postContents(content: string, model: string) {
+	const postModel = model;
+	const data: PostData = {
+		model: postModel,
+		article: content
+	};
+	return postData(`http://18.182.126.246/api/demo`, data);
+}
+
+function arrangePostData(url:string){
+
+}
+
+// GETメソッドの実装
+
+function postData(url = ``, data = {}) {
+	return fetch(url, {
+		method: "GET",
+		mode: "cors",
+		credentials: "same-origin",
+		headers: {
+			"Content-Type": "text/plain"
+		},
+		redirect: "follow",
+		referrer: "no-referrer",
+		body: JSON.stringify(data)
+	}).then(response => response.text());
+}
